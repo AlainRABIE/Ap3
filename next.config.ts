0c7353dfd5ed403c1d7f4ac7784c4117ebd92446
AlainR <rabie.alain001@gmail.com>
2025-01-31 08:58:49 +0100

    j'ai juste changée le next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import type { NextConfig } from "next";
-import type { Configuration } from "webpack";
-
-const nextConfig: NextConfig = {
-  output: "standalone", 
-
-  webpackDevMiddleware: (config: Configuration) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Assurez-vous que cette ligne est bien présente
   },
 };
 
